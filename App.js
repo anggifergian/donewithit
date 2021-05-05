980d3045b019f86344a0e0875286703ca61f7b6a
anggifergian <anggyfergian56@gmail.com>
2021-05-05 18:37:18 +0700

    Building the card component

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AppCard from "./app/components/AppCard";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 90,
+      }}
+    >
+      <AppCard
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
