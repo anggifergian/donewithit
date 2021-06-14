753577bbbb39973b6d16e1570e630678f81455a3
anggifergian <anggyfergian56@gmail.com>
2021-06-14 12:31:12 +0700

    Add new component

---
+++ app/components/ListItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
+import { Swipeable } from "react-native-gesture-handler";
+import colors from "../config/colors";
+import AppText from "./AppText";
+
+function ListItem({ title, subTitle, image, ImageComponent, onPress, renderRightActions }) {
+  return (
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailsContainer}>
+            <AppText style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center"
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
+  subTitle: {
+    fontWeight: "400",
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: "500",
+  },
+});
+
+export default ListItem;
