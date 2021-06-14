fadb2e73a47ebf0beb79b4dce86949a7e212b370
anggifergian <anggyfergian56@gmail.com>
2021-06-14 12:30:49 +0700

    Fixing App.js and app.json

--- App.js
+++ App.js
@@ -3,7 +3,11 @@ import { StyleSheet, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppCard from "./app/components/AppCard";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import MyAccountScreen from "./app/screens/MyAccountScreen";
+import ListingScreen from "./app/screens/ListingScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ListingScreen />;
 }
