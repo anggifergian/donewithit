a2ddd067ef8579976a078343caf8107a995b2d14
anggifergian <anggyfergian56@gmail.com>
2021-06-14 14:21:33 +0700

    Update AppText and add AppTextInput component

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, TextInput, StyleSheet, Platform } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import defaultStyles from "../config/styles";
+
+const AppTextInput = ({ icon, ...otherProps}) => {
+  return (
+    <View style={styles.container}>
+      {icon && <MaterialCommunityIcons name={icon} size={20} color={colors.medium} style={styles.icon} />}
+      <TextInput style={defaultStyles.text} {...otherProps} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    marginVertical: 10,
+    padding: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    width: '100%',
+  },
+  icon: {
+    marginRight: 12,
+  },
+})
+
+export default AppTextInput;
