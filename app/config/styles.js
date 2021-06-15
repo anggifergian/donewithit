2c3bff35e31e741cbb5693191c2855dd742e94bc
anggifergian <anggyfergian56@gmail.com>
2021-06-15 12:27:36 +0700

    Edit default styles

--- app/config/styles.js
+++ app/config/styles.js
@@ -3,6 +3,7 @@ import colors from "./colors";
 
 export default {
   text: {
+    width: "100%",
     color: colors.dark,
     fontSize: 16,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
