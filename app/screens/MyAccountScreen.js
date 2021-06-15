0586dac2b87b7c3486ca2726f9a537791023d8b7
anggifergian <anggyfergian56@gmail.com>
2021-06-15 13:01:10 +0700

    Adjust list component path

--- app/screens/MyAccountScreen.js
+++ app/screens/MyAccountScreen.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
 
 import Screen from '../components/Screen';
-import ListItem from '../components/ListItem';
 import Icon from '../components/Icon';
-import ListItemSeparator from '../components/ListItemSeparator';
 import colors from '../config/colors';
+import { ListItem, ListItemSeparator } from "../components/lists";
 
 const menuItems = [
   {
