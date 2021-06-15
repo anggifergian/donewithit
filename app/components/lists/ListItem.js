8b5ba8cf7778d982f5991923067bdbf3523b0dd9
anggifergian <anggyfergian56@gmail.com>
2021-06-15 12:51:54 +0700

    Organize lists component

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
 import { Swipeable } from "react-native-gesture-handler";
-import colors from "../config/colors";
-import AppText from "./AppText";
+import colors from "../../config/colors";
+import AppText from "../AppText";
 
 function ListItem({ title, subTitle, image, ImageComponent, onPress, renderRightActions }) {
   return (
