d9699356767fa2501f4078803756aee124e0623d
anggifergian <anggyfergian56@gmail.com>
2021-06-24 11:41:47 +0700

    Fix app.js

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import MyAccountScreen from "./app/screens/MyAccountScreen";
-import ListingScreen from "./app/screens/ListingScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <ListingScreen />;
+  return <ListingEditScreen />
 }
