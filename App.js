c3deb01b9e34f41b1e9a4057555a8ec434b22aad
anggifergian <anggyfergian56@gmail.com>
2021-06-25 19:10:57 +0700

    Add FormImagePicker on ListingEditScreen

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Screen from "./app/components/Screen";
+import ImageInputList from "./app/components/ImageInputList";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -9,12 +9,14 @@ import {
   SubmitButton
 } from "../components/forms";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import FormImagePicker from "../components/forms/FormImagePicker";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image.")
 });
 
 const categories = [
@@ -29,6 +31,7 @@ const initialValues = {
   price: "",
   description: "",
   category: null,
+  images: []
 }
 
 function ListingEditScreen() {
@@ -39,6 +42,7 @@ function ListingEditScreen() {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images" />
         <FormField
           name="title"
           placeholder="Title"
