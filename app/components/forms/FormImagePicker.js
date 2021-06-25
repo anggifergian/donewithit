56fa6cdd71045d487467dc7cdf2fa168d1c9e54f
anggifergian <anggyfergian56@gmail.com>
2021-06-25 19:10:37 +0700

    Create FormImagePicker, ImageInputList, and ImageInput

---
+++ app/components/forms/FormImagePicker.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useFormikContext } from "formik";
+
+import ImageInputList from '../ImageInputList';
+import ErrorMessage from './ErrorMessage';
+
+function FormImagePicker({ name }) {
+  const {
+    errors,
+    setFieldValue,
+    touched,
+    values
+  } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleRemove = uri => {
+    setFieldValue(name, imageUris.filter(imageUri => imageUri !== uri));
+  }
+
+  const handleAdd = uri => {
+    setFieldValue(name, [...imageUris, uri]);
+  }
+
+  return (
+    <>
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]}/>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default FormImagePicker;
