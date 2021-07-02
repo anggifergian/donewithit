efddddd1c6a23181d71af0d199b1db9972f17a11
anggifergian <anggyfergian56@gmail.com>
2021-07-02 17:11:56 +0700

    Fixing UI component

--- app/config/styles.js
+++ app/config/styles.js
@@ -4,7 +4,6 @@ import colors from "./colors";
 export default {
   colors,
   text: {
-    width: "100%",
     color: colors.dark,
     fontSize: 16,
     fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
