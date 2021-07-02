e39c645da703ea474b1a1ce93b2ad5dd78781e3e
anggifergian <anggyfergian56@gmail.com>
2021-07-02 17:12:03 +0700

    Fixing FormField

--- app/components/forms/FormField.js
+++ app/components/forms/FormField.js
@@ -5,13 +5,20 @@ import TextInput from "../TextInput";
 import ErrorMessage from "./ErrorMessage";
 
 function AppFormField({name, width, ...otherProps}) {
-  const {setFieldTouched, handleChange, touched, errors} = useFormikContext();
+  const {
+    setFieldTouched,
+    setFieldValue,
+    touched,
+    errors,
+    values
+  } = useFormikContext();
 
   return (
     <>
       <TextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={value => setFieldValue(name, value)}
+        value={values[name]}
         width={width}
         {...otherProps}
       />
