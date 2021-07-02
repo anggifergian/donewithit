6056e66eb05688910f68ab4b4eacbde426874791
anggifergian <anggyfergian56@gmail.com>
2021-07-02 17:10:43 +0700

    Add uploadScreen animation

---
+++ app/screens/UploadScreen.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, Modal } from 'react-native';
+import * as Progress from "react-native-progress";
+import LottieView from "lottie-react-native";
+
+import colors from '../config/colors';
+
+function UploadScreen({ onDone, progress = 0, visible = false }) {
+  return (
+    <Modal visible={visible}>
+      <View style={styles.container}>
+        {progress < 1 ? (
+          <Progress.Bar progress={progress} color={colors.primary} />
+        ) : (
+          <LottieView
+            autoPlay
+            loop={false}
+            onAnimationFinish={onDone}
+            source={require("../assets/animations/done.json")}
+            style={styles.animation}
+          />
+        )}
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  animation: {
+    width: 150
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
+
+export default UploadScreen;
